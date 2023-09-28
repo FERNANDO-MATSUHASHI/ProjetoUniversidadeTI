d0565aeeb3380667550d578af18133bf834134b6
HeitorSKimura <heitorskimura2010@hotmail.com>
2023-09-27 21:02:17 -0300

    First commit

---
+++ DDD.Domain/TIContext/Programador.cs
@@ -0,0 +1,17 @@
+﻿using DDD.Domain.SecretariaContext;
+using DDD.Domain.UserManagementContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.TI
+{
+    public class Programador : User
+    {
+        public string NivelAtuacao { get; set; }
+
+        public IList<Gerente>? Gerente { get; set; }
+    }
+}
